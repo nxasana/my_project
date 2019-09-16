6411467c74bf23807d415e4c90126e50b3763480
nxasana <ncukanaathandiwe@gmail.com>
2019-09-16 13:13:30 -0700

    Added a new Controller for sure

---
+++ src/AppBundle/Controller/GenusControllerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class GenusControllerController extends Controller
+{
+    /**
+     * @Route("/index")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:GenusController:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}
